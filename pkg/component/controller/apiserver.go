ce122ac2a17c969129cdc90e539e44391964aa4e
jasmingacic <jasmin.gacic@gmail.com>
2021-03-11 09:26:30 +0000

    1.2.35
    Signed-off-by: jasmingacic <jasmin.gacic@gmail.com>

--- pkg/component/controller/apiserver.go
+++ pkg/component/controller/apiserver.go
@@ -50,6 +50,7 @@ var apiDefaultArgs = map[string]string{
 	"requestheader-username-headers":     "X-Remote-User",
 	"secure-port":                        "6443",
 	"anonymous-auth":                     "false",
+	"tls-cipher-suites":                  "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
 }
 
 const egressSelectorConfigTemplate = `
